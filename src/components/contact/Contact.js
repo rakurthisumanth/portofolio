c81112dc6d80b909684ccd7262d9d0c57ca07e7f
sumanth <84782657+rakurthisumanth@users.noreply.github.com>
2022-01-16 12:08:53 +0530

    change contact

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -26,20 +26,22 @@ function Contact() {
         
     }
     return (
-        <div>
+          <div>
             <Heder/>
             <Grid container spacing={2}>
                 <Grid item lg={6}>
-                    <div style={{marginLeft:"8vh"}}>
+                    <div style={{marginLeft:"8vh"}}><br/>
                         <h1 style={{fontSize:"8vh",color:"white",marginBottom:"2vh"}}>Hire Me.</h1>
         <p style={{color:"white"}}>I am avalible for freelance Work. Connect with me via phone:</p><br/>
-        <p style={{color:"white",display:"flex"}}>Email:<p style={{color:"red"}}>Sumanth6633@gmail.com</p></p><br/><br/><br/><br/>
-        <Paper    style={{backgroundColor:"skyblue",color:"blue",width:"60vh",height:"50vh"}}><br/>
-           <div style={{marginLeft:"14vh"}}> <TextField value={name1} onChange={(e)=>setname1(e.target.value)} label="Your Name"/><br/><br/><br/> 
-            <TextField  id="margin-dense" margin="dense" value={name} onChange={(e)=>setname(e.target.value)} label="Your email"/><br/><br/><br/>
-            <TextField  value={sub} onChange={(e)=>setsub(e.target.value)} label="subject"/><br/><br/><br/>
+        <p style={{color:"white",display:"flex"}}>Email:<p style={{color:"red"}}>Sumanth6633@gmail.com</p></p><br/><br/>
+        <Paper    style={{backgroundColor:"black",color:"white",width:"60vh",height:"50vh"}}><br/>
+           <div style={{marginLeft:"14vh"}}>  <p>Your Name</p>  <TextField color="secondary" style={{width:"52vh",color:"red", backgroundColor:"skyblue"}} focused value={name1} onChange={(e)=>setname1(e.target.value)} /><br/><br/><br/> 
+           <p>Your Email</p>  <TextField style={{width:"52vh",color:"red", backgroundColor:"skyblue"}}    
+  value={name} onChange={(e)=>setname(e.target.value)}      color='secondary'  focused    /><br/><br/><br/>
+          <p>Subject</p>  <TextField  style={{width:"52vh",color:"red", backgroundColor:"skyblue"}} focused color='secondary'  value={sub} onChange={(e)=>{setsub(e.target.value);}} /><br/><br/><br/>
            <Button onClick={HandleSubmit} style={{marginLeft:"7vh"}} variant='contained'>Submit</Button><br/>
           <p style={{color:"red"}}>{er}</p> 
+        
             </div>
         </Paper>
         </div>
@@ -47,6 +49,7 @@ function Contact() {
      <Grid item lg={6}>
      <img style={{width:"90vh",height:"81vh"}} src={Pic}/>
      </Grid>
+    
 
             </Grid>
             </div>
